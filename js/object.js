7640bd34f2eeb6ab0991a787cad194df451e91aa
ahkamulbd <ahkamulbd@gmail.com>
2023-08-16 15:42:43 +0600

    Object in JavaScript addressed

---
+++ js/object.js
@@ -0,0 +1,43 @@
+// let a = 10;
+// let b = 20;
+
+// //console.log(`Before swap: Value of a = ${a} and Value of b = ${b}`);
+
+// function swapFunction(a, b) {
+//     //console.log(`Before Swap inside function: Value of a = ${a} and Value of b = ${b}`);
+
+//     let temp = a;
+//     a = b;
+//     b = temp;
+//     //console.log(`After Swap inside function: Value of a: ${a} and Value of b: ${b}`);
+// }
+
+// swapFunction(a, b);
+// //console.log(`After Swap: Value of a = ${a} and Value of b = ${b}`);
+
+
+/* ============ Above Program with Object Data Type ============ */
+
+// let obj = {
+//     a: 10,
+//     b: 20
+// }
+
+// console.log(`Before swap: Value of a = ${obj.a} and Value of b = ${obj.b}`);
+
+// function swapFunction(x) {
+//     console.log(`Before Swap inside function: Value of a = ${x.a} and Value of b = ${x.b}`);
+
+//     let temp = x.a;
+//     x.a = x.b;
+//     x.b = temp;
+//     console.log(`After Swap inside function: Value of a: ${x.a} and Value of b: ${x.b}`);
+// }
+
+// swapFunction(obj);
+// console.log(`After Swap: Value of a = ${obj.a} and Value of b = ${obj.b}`);
+
+let array = [10, 20, 30, 40, 50];
+
+//console.dir(array);
+console.log({});

87fcb0cc128354424117928c621112d65ae7bb3e
ahkamulbd <ahkamulbd@gmail.com>
2023-08-20 12:41:06 +0600

    Strict Mode addressed

---
+++ js/strictMode.js
@@ -0,0 +1,13 @@
+'use strict'
+
+let name = 'Amima Hoque';
+let age = 8;
+let address = 'Lama';
+console.log(`My name is ${name} and I am ${age} years old.`);
+
+function newFunction() {
+    job = 'Student';
+    console.log('A Function with Strict Mode Enabled');
+}
+newFunction();
+console.log(`I am a ${job} And I am from ${address}`);
